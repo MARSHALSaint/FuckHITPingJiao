777e6fdad6c9d1d44ac8d13430887052a8a57b66
hit9 <nz2324@126.com>
2013-06-11 16:14:19 +0800

    3 steps ok!

--- index.py
+++ index.py
@@ -2,13 +2,97 @@
 
 import re
 import requests
+from random import randint, shuffle
 from flask import Flask, request
 
 app = Flask("FuckPingJiao")
 
 
+# build a regex pattern to fetch the hidden inputs out of form
+pattern = r'<input\s+name="(?P<name>[^"]+)"[^>]+value="(?P<value>[^"]+)"\s*>'
+
+
+def step1(cookies):
+    """The first step: http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJJS4.asp"""
+    # get the html of form
+    uri_get = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJJS4.asp"
+    res = requests.get(uri_get, cookies=cookies)
+    html = res.text  # repsonse html
+
+    data = {}  # build our post data
+    lst = re.findall(pattern, html)  # find out the inputs
+    data.update(dict((x[0], x[1]) for x in lst))
+    # how many his courses are
+    number = int(data["ALLRS"])
+    for i in range(1, number+1):
+        for j in range(1, 8):
+            data["PJ"+str(i)+"_"+str(j)] = randint(3, 5)
+        data["PJ"+str(i)+"_8"]  = ""
+        data["PJ"+str(i)+"_9"]  = ""
+    # url to post to
+    uri_post = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJJS4_Save.asp"
+    res = requests.post(uri_post, data=data, cookies=cookies)
+    return res
+
+
+def step2(cookies):
+    """The 2nd step: http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJKC4.asp"""
+    # get the html of form
+    uri_get = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJKC4.asp"
+    res = requests.get(uri_get, cookies=cookies)
+    html = res.text  # repsonse html
+
+    data = {}  # build our post data
+    lst = re.findall(pattern, html)  # find out the inputs
+    data.update(dict((x[0], x[1]) for x in lst))
+    # how many his courses are
+    number = int(data["ALLRS"])
+    for i in range(1, number+1):
+        for j in range(10, 15):
+            data["PJ"+str(i)+"_"+str(j)] = randint(3, 5)
+        data["PJ"+str(i)+"_15"] = str(randint(80, 95))
+    # url to post to
+    uri_post = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJKC4_Save.asp"
+    res = requests.post(uri_post, data=data, cookies=cookies)
+    return res
+
+
+def step3(cookies):
+    """The 3rd step: http://xscj.hit.edu.cn/hitjwgl/xs/pj/ZHPJ4.asp"""
+
+    uri_get = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/ZHPJ4.asp"
+    res = requests.get(uri_get, cookies=cookies)
+    html = res.text  # repsonse html
+
+    data = {}  # build our post data
+    lst = re.findall(pattern, html)  # find out the inputs
+    data.update(dict((x[0], x[1]) for x in lst))
+    # how many his courses are
+    number = int(data["ALLRS"])
+
+    # best: 0-1 teacher
+    # good: 2-4 teachers
+
+    # make a list of number size
+    l = [0] * number
+    l[0] = 6
+    l[1] = l[2] = l[3] = l[4] =  5
+    for i in range(5, number):
+        l[i] = 4
+    # shuffle it
+    shuffle(l)
+
+    for i in range(0, number):
+        data["PJ"+str(i+1)] = l[i]
+    # url to post to
+    uri_post = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/ZHPJ4_Save.asp"
+    res = requests.post(uri_post, data=data, cookies=cookies)
+    return res
+
+
 @app.route("/")
 def main():
+    # --------------- Create Cookies  -------------------
     # get student id from url's parameters
     sid = request.args.get("SID")
     # read cookies string and create a new one
@@ -25,34 +109,11 @@ def main():
     cdct["UserID"] = sid
     cdct["UserClass"] = "111111"
 
-    # get the html of form
-    uri_get = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJJS4.asp"
-    resp = requests.get(uri_get, cookies=cdct)
-    html = resp.text  # repsonse html
-
-    # how many his courses are
-    number = html.count("</tr>") - 6
-
-    data = {}  # build our post data
-
-    for i in range(1, number+1):
-        for j in range(1, 8):
-            data["PJ"+str(i)+"_"+str(j)] = j % 2 + 4
-        data["PJ"+str(i)+"_8"]  = ""
-        data["PJ"+str(i)+"_9"]  = ""
-
-    # build a regex pattern to fetch the hidden inputs out of form
-    pattern = r'<input\s+name="(?P<name>[^"]+)"[^>]+value="(?P<value>[^"]+)"\s*>'
-
-    lst = re.findall(pattern, html)  # find out the inputs
-
-    data.update(dict((x[0], x[1]) for x in lst))
-    # this is what, the number
-    data["ALLRS"] = str(number)
-    # url to post to
-    uri_post = "http://xscj.hit.edu.cn/hitjwgl/xs/pj/PJJS4_Save.asp"
-    res = requests.post(uri_post, data=data, cookies=cdct)
-    return res.text
+    # ----------  End cookies creation -----------------
+    re_1 = step1(cdct)
+    re_2 = step2(cdct)
+    re_3 = step3(cdct)
+    return "ok"
 
 
 if __name__ == '__main__':
